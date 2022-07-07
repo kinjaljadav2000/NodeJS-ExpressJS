b5df8dfef007aec62ef8637afe0c9fd43423f093
kinjaljadav2000 <95681415+kinjaljadav2000@users.noreply.github.com>
2022-07-07 22:54:41 +0530

    Create countCode.js

---
+++ string-2/countCode.js
@@ -0,0 +1,9 @@
+function countCode(str){
+  let count = 0;  
+    for(let i = 0; i < str.length - 3; i++) {
+        if(str.substring(i, i + 2)==("co") && str.charAt(i + 3) == 'e')
+            count++;
+    }    
+    return count;
+}
+}
