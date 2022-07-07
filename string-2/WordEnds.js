e1d4275f81bcea5b33cd7917ce9d4bbeba4e84b0
kinjaljadav2000 <95681415+kinjaljadav2000@users.noreply.github.com>
2022-07-07 23:35:05 +0530

    Create WordEnds.js

---
+++ string-2/WordEnds.js
@@ -0,0 +1,14 @@
+function wordEnds(str, word){
+   slen = str.length;
+  wlen = word.length;
+  fin = "";
+  for ( i = 0; i < slen-wlen+1; i++) {
+     tmp = str.substring(i,i+wlen);
+    if (i > 0 && tmp==(word))
+      fin += str.substring(i-1,i);
+    if (i < slen-wlen && tmp==(word))
+      fin += str.substring(i+wlen,i+wlen+1);
+  }
+  return fin;
+}
+}
