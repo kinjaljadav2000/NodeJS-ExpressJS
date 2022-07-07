6f63ffe27164f16e8b73e43499432cec08d9e0b1
kinjaljadav2000 <95681415+kinjaljadav2000@users.noreply.github.com>
2022-07-07 23:19:30 +0530

    Create XYbalance.js

---
+++ string-2/XYbalance.js
@@ -0,0 +1,19 @@
+function xyBalance(str){  
+let x = false;
+let y = false;
+  let len = str.length;
+  for (let i = 0; i < len; i++) {
+    if (str.charAt(i) == 'x' && y == true){
+      x = true;
+      y = false;
+    } else if (str.charAt(i) == 'x') {
+      x = true;
+  }
+    if (str.charAt(i) == 'y' && x == true)
+      y = true;
+  }
+  if (x == false)
+  y = true;
+  return y;
+}
+}
