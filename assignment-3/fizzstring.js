ce896f992ccb71c10d0e37a33079ba1e6ec64146
kinjaljadav2000 <95681415+kinjaljadav2000@users.noreply.github.com>
2022-07-07 21:52:08 +0530

    Create fizzstring.js

---
+++ assignment-3/fizzstring.js
@@ -0,0 +1,11 @@
+function fizzString(str){
+  if(str.length > 0 && 
+        str.charAt(0) == 'f' && str.charAt(str.length - 1) == 'b')
+        return "FizzBuzz";      
+    if(str.length > 0 && str.charAt(0) == 'f')
+        return "Fizz";
+  if(str.length > 0 && str.charAt(str.length - 1) == 'b')
+        return "Buzz";
+        return str;
+}  
+}
