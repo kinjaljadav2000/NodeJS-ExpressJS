8d38416344a8713b565f5824a2cd9af70e70c907
kinjaljadav2000 <95681415+kinjaljadav2000@users.noreply.github.com>
2022-07-08 21:29:29 +0530

    Update maxSpan.js

--- Array-3/maxSpan.js
+++ Array-3/maxSpan.js
@@ -1,3 +1,13 @@
 function maxSpan(nums){
-  
+  let max = 0;
+ for (let i = 0; i < nums.length; i++) {
+    let j = nums.length - 1;
+    while (nums[i] != nums[j]) {
+      j--;
+    }
+    let span = j - i + 1;
+    if (span > max)
+      max = span;
+  }
+  return max;
 }
